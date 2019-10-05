8f3b85ff6f26808935d3f57072516eb5ce7a9bbb
Brett Hazen <github@javajolt.com>
2019-10-05 09:36:00 -0600

    Add in mix boilerplate

---
+++ lib/cascade.ex
@@ -0,0 +1,18 @@
+defmodule Cascade do
+  @moduledoc """
+  Documentation for Cascade.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cascade.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
